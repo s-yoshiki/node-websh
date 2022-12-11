8528ff6d61a2100afefba662584b3d7c306d7408
Yoshiki Shinagawa <s.yoshiki1123@gmail.com>
2022-12-11 18:11:35 +0900

    feat: add xterm addons

--- xterm_profile.js
+++ xterm_profile.js
@@ -1,9 +1,30 @@
-const fitAddon = new FitAddon.FitAddon();
+// const { Terminal } = require("xterm");
+
 const term = new Terminal({
   cols: 80,
   rows: 24,
+  allowProposedApi: true,
 });
-term.loadAddon(fitAddon);
+term.open(document.getElementById("terminal"));
+
+// addons
+const fitAddon = new FitAddon.FitAddon();
+// const ligaturesAddon = new LigaturesAddon.LigaturesAddon();
+const searchAddon = new SearchAddon.SearchAddon(); 
+const webLinksAddon = new WebLinksAddon.WebLinksAddon(); 
+const unicode11Addon = new Unicode11Addon.Unicode11Addon(); 
+const serializeAddon = new SerializeAddon.SerializeAddon(); 
+
+[
+  fitAddon,
+  // ligaturesAddon,
+  searchAddon,
+  webLinksAddon,
+  unicode11Addon,
+  serializeAddon,
+].map((e) => term.loadAddon(e));
+
+term.unicode.activeVersion = '11';
 
 const ws = new WebSocket(`ws://${location.hostname}:8999`);
 
@@ -14,13 +35,14 @@ ws.addEventListener("message", function (event) {
   console.debug("Message from server ", event.data);
   try {
     let output = JSON.parse(event.data);
-    term.write(output.output);
+    term.write(output.output, () => {
+      console.log(serializeAddon.serialize());
+    });
   } catch (e) {
     console.error(e);
   }
 });
 
-term.open(document.getElementById("terminal"));
 
 term.onData((data) => ws.send(JSON.stringify({ input: data })));
 
@@ -32,6 +54,6 @@ fitAddon.fit();
 
 term.onResize((size) => {
   console.debug("resize");
-  let resizer = JSON.stringify({ resizer: [size.cols, size.rows] });
+  const resizer = JSON.stringify({ resizer: [size.cols, size.rows] });
   ws.send(resizer);
 });
